635c4b9122549b3e70e24a09a5984147a58574ff
Daniel Moreno <daniel.moreno@grupoadmark.com>
2021-09-08 11:14:48 +0200

    IOS y SW

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ console.log("entro en el SW");
 
 //creo las constante de los nombres de nuestas caches
 
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 //creo APP_SHELL de los archivos propios
@@ -69,6 +69,10 @@ self.addEventListener('activate', e => {
 
         keys.forEach(key => {
             if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);
+            }
+
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key);
             }
         })
